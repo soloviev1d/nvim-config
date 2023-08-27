d675b1625e525bfd4cfd27c59b2cee8177fc66b4
Daniil <soloviev.dm11@gmail.com>
2023-08-27 16:54:08 +0300

    use another plugin for go support

--- init.lua
+++ init.lua
@@ -220,15 +220,31 @@ require('lazy').setup({
     end,
   },
 
+  -- {
+  --   "olexsmir/gopher.nvim",
+  --   ft = "go",
+  --   config = function(_, opts)
+  --     require("gopher").setup(opts)
+  --   end,
+  --   build = function()
+  --     vim.cmd [[silent! GoInstallDeps]]
+  --   end,
+  -- },
+  --
+  --
   {
-    "olexsmir/gopher.nvim",
-    ft = "go",
-    config = function(_, opts)
-      require("gopher").setup(opts)
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
     end,
-    build = function()
-      vim.cmd [[silent! GoInstallDeps]]
-    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
   },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
